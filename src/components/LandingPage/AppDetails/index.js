6626c7cee814dcb83b7b48f9b5c101cb108a27f2
Shemeer <shameerbem@gmail.com>
2023-07-20 14:09:49 +0530

    modified commit

--- src/components/LandingPage/AppDetails/index.js
+++ src/components/LandingPage/AppDetails/index.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { motion } from 'framer-motion';
 import './style.css';
 
 function AppDetails({ img, heading, Description, margin, align }) {
 
+    const [width,setWidth] = useState(window.screen.width);
+
+    
+    useEffect(()=>{
+        window.addEventListener('resize',()=>{
+           setWidth(window.screen.width);
+        });
+
+        return()=>{
+            window.removeEventListener('resize',()=>{
+                setWidth(window.screen.width);
+            })
+        }
+    },[width]);
 
 
-    return align === true ? (
+    return align === true && width > 1020 ? (
         <motion.div
             initial={{ opacity: 0, y: 50 }}
             whileInView={{ opacity: 1, y: 0 }}
@@ -14,14 +28,14 @@ function AppDetails({ img, heading, Description, margin, align }) {
             className='appDetails'
             style={{ marginTop: margin }}>
             <div className='left'>
-                <img src={img} alt='img' width={"80%"} height={"300px"} />
+                <img src={img} alt='img'/>
             </div>
             <div className='right'>
                 <h1>{heading}</h1>
                 <p className='para'>{Description}</p>
             </div>
         </motion.div>
-    ) :
+    ) :width > 1020?
         (
             <motion.div
                 initial={{ opacity: 0, y: 50 }}
@@ -34,7 +48,22 @@ function AppDetails({ img, heading, Description, margin, align }) {
                     <p className='para'>{Description}</p>
                 </div>
                 <div className='left'>
-                    <img src={img} alt='img' width={"80%"} height={"300px"} />
+                    <img src={img} alt='img'/>
+                </div>
+            </motion.div>
+        ):(
+            <motion.div
+                initial={{ opacity: 0, y: 50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5, delay: 0.5 }}
+                className='appDetails'
+                style={{ marginTop: margin }}>
+                <div className='left'>
+                    <img src={img} alt='img'/>
+                </div>
+                <div className='right'>
+                    <h1>{heading}</h1>
+                    <p className='para'>{Description}</p>
                 </div>
             </motion.div>
         )

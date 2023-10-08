c4fba2656b3a1e02108856eef14b731d8b122003
Shemeer <shameerbem@gmail.com>
2023-10-08 21:53:55 +0530

    updated commit

--- src/components/Common/Footer/index.js
+++ src/components/Common/Footer/index.js
@@ -17,7 +17,7 @@ function Footer() {
   return (
     <div className='footer'>
         <h2 className="logo" onClick={() => topFunction()}>
-        CryptoTracker<span>.</span>
+        CoinWatchr<span>.</span>
       </h2>
       <div className="social-links">
         <a href="https://facebook.com">
